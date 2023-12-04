b33fdd2c2556b40b4a8fa777e181096a95de63be
FrancoCaredda <franco.caredda12345@gmail.com>
2023-12-04 16:41:50 +0100

    Add CreateBooking function

--- app/dashboard/webapp/controller/AllBookings.controller.js
+++ app/dashboard/webapp/controller/AllBookings.controller.js
@@ -1,13 +1,19 @@
 sap.ui.define([
     "flight/app/dashboard/controller/BaseController",
     "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
     "flight/app/dashboard/utils/BookingServiceFacade",
     "flight/app/dashboard/model/formatter"
-], function(Controller, MessageBox, BookingServiceFacade, formatter) {
+], function(Controller, MessageBox, JSONModel, BookingServiceFacade, formatter) {
     'use strict';
     
     return Controller.extend("flight.app.dashboard.controller.AllBookings", {
         formatter: formatter,
+        onInit: function(oEvent) {
+            this.getView().setModel(new JSONModel({
+                createBookingDialog: {}
+            }), "viewModel");
+        },
         onDeleteBookingButtonPressed: async function(oEvent) {
             const oDataModel = this.getOwnerComponent().getModel();
             const oTable = this.getView().byId("BookingsTable");
@@ -30,6 +36,48 @@ sap.ui.define([
                 oEventBus.publish("refreshUserDetail", "AllBookingsToDetailPage", null);
             } catch (e) { 
                 MessageBox.error((e.message) ? e.message : e);
+            }
+        },
+        onCreateBookingButtonPressed: async function(oEvent) {
+            if (!this.__createBookingDialog) {
+                this.__createBookingDialog = await this.loadFragment({
+                    name: "flight.app.dashboard.view.fragments.dialog.CreateBookingDialog"
+                });
+            }
+
+            this.__createBookingDialog.open();
+        },
+        __clearCreateBookingDialogData: function() {
+            const oViewModel = this.getView().getModel("viewModel");
+            oViewModel.setProperty("/createBookingDialog", {});
+            oViewModel.refresh();
+        },
+        onCreateBookingDialogClosed: function(oEvent) {
+            this.__clearCreateBookingDialogData();
+            this.__createBookingDialog.close();
+        },
+        onCreateBookingDialogConfirmPressed: async function(oEvent) {
+            const oViewModel = this.getView().getModel("viewModel");
+            const oDataModel = this.getOwnerComponent().getModel();
+
+            let oData = oViewModel.getProperty("/createBookingDialog");
+            oData.airlineID = oData.airlineID.toUpperCase();
+            oData.user_ID = this.getView().getBindingContext().getProperty("ID");
+            
+            try {
+                await BookingServiceFacade.createEntity(oDataModel,
+                    oData, 
+                    "/Bookings");
+                    
+                MessageBox.success(this.getText("userdetail.objectpage.section1.subsection1.table.action1.success"));
+
+                const oEventBus = sap.ui.getCore().getEventBus();
+                oEventBus.publish("refreshUserDetail", "AllBookingsToDetailPage", null);
+            } catch(e) {
+                MessageBox.error((e.message) ? e.message : e);
+            } finally {
+                this.__clearCreateBookingDialogData();
+                this.__createBookingDialog.close();
             }
         }
     });
